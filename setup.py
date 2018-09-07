b00a6dc7fd91bfd8c804294bb59795973ac58444
Rusty Mann <mannrusty@gmail.com>
2018-09-07 11:40:48 -0700

    changed all date formats to dashes rather than slashes to avoid issue with heroku.

--- setup.py
+++ setup.py
@@ -10,16 +10,16 @@ db.drop_tables([User, Bill])
 
 db.create_tables([User, Bill])
 
-rent = Bill(name="Rent", amount=1600, first_day="2018/01/01", last_day="2018/01/31")
+rent = Bill(name="Rent", amount=1600, first_day="2018-01-01", last_day="2018-01-31")
 rent.save()
 
-electricity = Bill(name="Electricity", amount=100, first_day="2018/01/05", last_day="2018/02/04")
+electricity = Bill(name="Electricity", amount=100, first_day="2018-01-05", last_day="2018-02-04")
 electricity.save()
 
-dennis = User(username="Dennis", move_in="2018/01/01", move_out="2018/01/31")
+dennis = User(username="Dennis", move_in="2018-01-01", move_out="2018-01-31")
 dennis.save()
 
-mac = User(username="Mac", move_in="2018/01/05", move_out="2018/02/15")
+mac = User(username="Mac", move_in="2018-01-05", move_out="2018-02-15")
 mac.save()
 
 # charlie = User(username="Charlie", move_in="2018/01/01", move_out="2018/01/25")
